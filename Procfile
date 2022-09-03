95c48bacf1d4f0662738c2778604c8938b903aac
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-09-03 12:59:16 +0530

    done project

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn budget.wsgi --log-file -
+release: python manage.py migrate
--- budget/settings.py
+++ budget/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +124,8 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS=[
 #     os.path.join(BASE_DIR,'static')
 # ]
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
